74ea202142cfc01894549b7932d5f7076adde7d3
TImofey Semenov <semetima@mail.ru>
2024-01-20 07:27:21 +0300

    OK2!

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.List;
 
@@ -15,10 +15,10 @@ import java.util.List;
 @RequestMapping("/cars")
 public class CarsController {
 
-    private final CarService carService;
+    private final CarServiceImpl carService;
 
     @Autowired
-    public CarsController(CarService carService) {
+    public CarsController(CarServiceImpl carService) {
         this.carService = carService;
     }
 
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,27 @@
+package web.service;
+
+import web.models.Car;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private final List<Car> CARS = List.of(
+            new Car("Toyota Silvia", 23423432, 1997),
+            new Car("Toyota Camry", 324234, 2013),
+            new Car("Land Cruiser", 213214, 2019),
+            new Car("Lada Vesta", 324234, 2023),
+            new Car("Lada 2106", 213432, 2004)
+    );
+
+    @Override
+    public List<Car> getCars() {
+        return CARS;
+    }
+
+    public List<Car> getCars(Integer count) {
+        return (count > CARS.size()) ? CARS : CARS.subList(0, count);
+    }
+
+}
